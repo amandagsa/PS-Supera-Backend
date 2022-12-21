ae5bc22e37c9a0723fa98cba1ea94fc41b8760d2
unknown <amandagsal@gmail.com>
2022-12-21 15:19:27 -0300

    Implementa repositorios

---
+++ src/main/java/br/com/banco/repository/TransferenciaRepository.java
@@ -0,0 +1,25 @@
+package br.com.banco.repository;
+
+import br.com.banco.entity.Transferencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
+
+    @Query("SELECT t FROM Transferencia t WHERE t.conta.id =?1")
+    public List<Transferencia> findAllByConta(Long idConta);
+
+    @Query("SELECT t FROM Transferencia t WHERE t.conta.id =?1 AND t.dataTransferencia BETWEEN ?2 AND ?3")
+    public List<Transferencia> findAllByAccontAndDate(Long idConta,LocalDateTime dataInicio, LocalDateTime dataFim);
+
+    @Query("SELECT t FROM Transferencia t WHERE t.conta.id =?1 AND t.nomeOperadorTransacao =?2")
+    public List<Transferencia> findAllByAccountAndOperator(Long idConta, String nomeOperadorTransacao);
+
+    @Query("SELECT t FROM Transferencia t WHERE t.conta.id =?1 AND t.dataTransferencia BETWEEN ?2 AND ?3 AND t.nomeOperadorTransacao =?4")
+    public List<Transferencia> findAllByAccountAndDateAndOperator(Long idConta, LocalDateTime dataInicio, LocalDateTime dataFim, String nomeOperadorTransacao);
+}
