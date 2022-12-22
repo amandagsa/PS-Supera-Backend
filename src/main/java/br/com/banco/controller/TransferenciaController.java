40ff2644483f31bca70ded0db3011ac30ebac0d8
unknown <amandagsal@gmail.com>
2022-12-22 10:31:13 -0300

    Refatora controller unificando endpoints

--- src/main/java/br/com/banco/controller/TransferenciaController.java
+++ src/main/java/br/com/banco/controller/TransferenciaController.java
@@ -1,18 +1,14 @@
 package br.com.banco.controller;
 
-import br.com.banco.service.ContaService;
 import br.com.banco.service.TransferenciaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "transferencia")
 public class TransferenciaController {
@@ -21,38 +17,14 @@ public class TransferenciaController {
     TransferenciaService transferenciaService;
 
     @GetMapping("/{id}")
-    public ResponseEntity findTransfersByAccountId(@PathVariable(value = "id") Long id) {
-        return ResponseEntity.status(HttpStatus.OK).body(transferenciaService.findByContaId(id));
-    }
+    public ResponseEntity findTransfersByAccountId(@PathVariable(value = "id") Long id,
+                                                   @RequestParam(required = false, defaultValue = "1970-01-01T01:01:01", name = "dataInicial") String dataInicial,
+                                                   @RequestParam(required = false, defaultValue = "#{T(java.time.LocalDateTime).now().toString()}", name = "dataFinal") String dataFinal,
+                                                   @RequestParam(required = false, defaultValue = "", name = "operador") String operador) {
 
-    @GetMapping("/{id}/{Dinicial}/{Dfinal}")
-    public ResponseEntity findTransfersByAccountIdAndDate(@PathVariable(value = "id") Long id,
-                                                          @PathVariable(value = "Dinicial") String Dinicial,
-                                                          @PathVariable(value = "Dfinal") String Dfinal) {
+        LocalDateTime dataInicio = LocalDateTime.parse(dataInicial);
+        LocalDateTime dataFim = LocalDateTime.parse(dataFinal);
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH:mm:ss");
-        LocalDateTime dataInicio = LocalDateTime.parse(Dinicial, formatter);
-        LocalDateTime dataFim = LocalDateTime.parse(Dfinal, formatter);
-
-        return ResponseEntity.status(HttpStatus.OK).body(transferenciaService.findByContaIdAndDate(id, dataInicio, dataFim));
-    }
-
-    @GetMapping("/{id}/{operador}")
-    public ResponseEntity findTransfersByAccountIdAndOperator(@PathVariable(value = "id") Long id,
-                                                              @PathVariable(value = "operador") String operador) {
-        return ResponseEntity.status(HttpStatus.OK).body(transferenciaService.findByContaIdAndOperator(id, operador));
-    }
-
-    @GetMapping("/{id}/{Dinicial}/{Dfinal}/{operador}")
-    public ResponseEntity findTransfersByAccountIdAndDateAndOperator(@PathVariable(value = "id") Long id,
-                                                                     @PathVariable(value = "Dinicial") String Dinicial,
-                                                                     @PathVariable(value = "Dfinal") String Dfinal,
-                                                                     @PathVariable(value = "operador") String operador) {
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH:mm:ss");
-        LocalDateTime dataInicio = LocalDateTime.parse(Dinicial, formatter);
-        LocalDateTime dataFim = LocalDateTime.parse(Dfinal, formatter);
-
-        return ResponseEntity.status(HttpStatus.OK).body(transferenciaService.findByContaIdAndDateAndOperator(id, dataInicio, dataFim, operador));
+        return ResponseEntity.status(HttpStatus.OK).body(transferenciaService.verifyHasOperator(id, dataInicio, dataFim, operador));
     }
 }
