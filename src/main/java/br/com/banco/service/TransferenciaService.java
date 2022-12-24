e6616e0668b324f644d4462e68ddcc6c246e5c32
Amanda Alves <amandagsal@gmail.com>
2022-12-24 16:56:31 -0300

    Remove import não utilizado

--- src/main/java/br/com/banco/service/TransferenciaService.java
+++ src/main/java/br/com/banco/service/TransferenciaService.java
@@ -4,7 +4,6 @@ import br.com.banco.entity.Transferencia;
 import br.com.banco.repository.TransferenciaRepository;
 import br.com.banco.response.ResponseTransferencia;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
