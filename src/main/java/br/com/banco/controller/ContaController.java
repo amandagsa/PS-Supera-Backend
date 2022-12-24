2d65db40f93632c556f34f86c9e12f6b03878941
unknown <amandagsal@gmail.com>
2022-12-24 11:25:02 -0300

    Implementa get para saldo de conta

--- src/main/java/br/com/banco/controller/ContaController.java
+++ src/main/java/br/com/banco/controller/ContaController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequestMapping(value = "conta")
 public class ContaController {
@@ -13,7 +15,7 @@ public class ContaController {
     @Autowired
     ContaService contaService;
 
-    @GetMapping("/all")
+    @GetMapping
     public ResponseEntity findAllAccounts() {
         return ResponseEntity.status(HttpStatus.OK).body(contaService.findAll());
     }
@@ -21,5 +23,16 @@ public class ContaController {
     @GetMapping("/{id}")
     public ResponseEntity findAccountById(@PathVariable(value = "id") Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(contaService.findById(id));
+    }
+
+    @GetMapping("/{id}/saldo")
+    public ResponseEntity findTotal(@PathVariable(value = "id") Long idConta,
+                                    @RequestParam(required = false, defaultValue = "1970-01-01T01:01:01", name = "dataInicial") String dataInicial,
+                                    @RequestParam(required = false, defaultValue = "#{T(java.time.LocalDateTime).now().toString()}", name = "dataFinal") String dataFinal) {
+
+        LocalDateTime dataInicio = LocalDateTime.parse(dataInicial);
+        LocalDateTime dataFim = LocalDateTime.parse(dataFinal);
+
+        return ResponseEntity.status(HttpStatus.OK).body(contaService.findTotal(idConta, dataInicio, dataFim));
     }
 }
--- src/main/java/br/com/banco/service/ContaService.java
+++ src/main/java/br/com/banco/service/ContaService.java
@@ -1,11 +1,14 @@
 package br.com.banco.service;
 
 import br.com.banco.entity.Conta;
+import br.com.banco.entity.Transferencia;
 import br.com.banco.error.ResourcesNotFoundException;
 import br.com.banco.repository.ContaRepository;
+import br.com.banco.repository.TransferenciaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -14,15 +17,28 @@ public class ContaService {
     @Autowired
     ContaRepository contaRepository;
 
+    @Autowired
+    TransferenciaService transferenciaService;
+
     private String MESSAGE = "Conta não encontrada para o ID";
 
     public List<Conta> findAll() {
         return contaRepository.findAll();
     }
 
-    public Conta findById(Long id) {
-        return contaRepository.findById(id).orElseThrow(() -> {
+    public Conta findById(Long idConta) {
+        return contaRepository.findById(idConta).orElseThrow(() -> {
             throw new ResourcesNotFoundException(MESSAGE);
         });
+    }
+
+    public Double findTotal(Long idConta, LocalDateTime dataInicio, LocalDateTime dataFim) {
+        List<Transferencia> listaTransferencias = transferenciaService.findByAccountIdAndDate(idConta, dataInicio, dataFim);
+        Double valorTotal = 0.0;
+
+        for (Transferencia transferencia : listaTransferencias) {
+            valorTotal += transferencia.getValor();
+        }
+        return valorTotal;
     }
 }
