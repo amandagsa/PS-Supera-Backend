9f85326b9857073d394822cd0f188269a5a6ecf3
unknown <amandagsal@gmail.com>
2022-12-24 16:12:40 -0300

    Remove endpoint de consulta de saldo da controller

--- src/main/java/br/com/banco/controller/ContaController.java
+++ src/main/java/br/com/banco/controller/ContaController.java
@@ -6,8 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-
 @RestController
 @RequestMapping(value = "conta")
 public class ContaController {
@@ -23,16 +21,5 @@ public class ContaController {
     @GetMapping("/{id}")
     public ResponseEntity findAccountById(@PathVariable(value = "id") Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(contaService.findById(id));
-    }
-
-    @GetMapping("/{id}/saldo")
-    public ResponseEntity findTotal(@PathVariable(value = "id") Long idConta,
-                                    @RequestParam(required = false, defaultValue = "1970-01-01T01:01:01", name = "dataInicial") String dataInicial,
-                                    @RequestParam(required = false, defaultValue = "#{T(java.time.LocalDateTime).now().toString()}", name = "dataFinal") String dataFinal) {
-
-        LocalDateTime dataInicio = LocalDateTime.parse(dataInicial);
-        LocalDateTime dataFim = LocalDateTime.parse(dataFinal);
-
-        return ResponseEntity.status(HttpStatus.OK).body(contaService.findTotal(idConta, dataInicio, dataFim));
     }
 }
